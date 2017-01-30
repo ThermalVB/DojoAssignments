2e07a38700280f4782241c47b8a31c8a1d6542c4
ThermalVB <wyatt.horejsi@gmail.com>
2017-01-29 17:20:06 -0800

    added js assignments 1/29/17

---
+++ WebFundamentals/Javascript/forAFewBillion.js
@@ -0,0 +1,6 @@
+forAFewBillion(days){
+  var money = 0.01;
+  for(var i = 0; i <= days; i++){
+    money = money * 2;
+  }
+}
---
+++ WebFundamentals/Javascript/whileYouWait.js
@@ -0,0 +1,16 @@
+function daysUntilMyBirthday(days){
+  if(days > 30){
+    console.log(days + " days until my birthday. Such a long time :(")
+  }
+  else if(days <= 30 && days > 5){
+    console.log(days + "days until my birthday! Almost there :)")
+  }
+  else if(days <= 5 && days > 0){
+    console.log(days + " DAYS UNTIL MY BIRTHDAY WOOOOOOO!!!ONE")
+  }
+  else{
+    console.log("♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪ღ♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•*")
+    console.log("♪ღ♪░H░A░P░P░Y░ B░I░R░T░H░D░A░Y░░♪ღ♪")
+    console.log("*•♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•«")
+  }
+}
